d118caded849121cef59dc550ca6c7efc0ba22c0
parham <parhamqop@gmail.com>
2025-10-01 11:15:44 +0330

    برگشتن کارت به جای اولش
    الان کارته هرجا ول کنی برمیگرده جای اولش

--- strategyCardGame/Assets/Scripts/Drag And Drop Test.cs
+++ strategyCardGame/Assets/Scripts/Drag And Drop Test.cs
@@ -5,8 +5,11 @@ public class DragAndDropTest : MonoBehaviour
     private Vector3 offset;
     private Camera cam;
 
+    Vector3 defaultPostion;
+
     void Awake()
     {
+        defaultPostion = transform.position;
         cam = Camera.main;
     }
 
@@ -14,13 +17,24 @@ public class DragAndDropTest : MonoBehaviour
     {
         Vector3 mouseWorld = cam.ScreenToWorldPoint(Input.mousePosition);
         mouseWorld.z = transform.position.z;
-        offset = transform.position - mouseWorld;
+        offset = this.transform.position - mouseWorld;
     }
 
     void OnMouseDrag()
     {
         Vector3 mouseWorld = cam.ScreenToWorldPoint(Input.mousePosition);
         mouseWorld.z = transform.position.z;
-        transform.position = mouseWorld - offset;
+        this.transform.position = mouseWorld + offset;
+
+        if (true)
+        {
+            Invoke("CardHandeler", 1);
+        }
+
+    }
+
+    void CardHandeler()
+    {
+        this.transform.position = defaultPostion;
     }
 }
